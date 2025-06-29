4288c766443ab1bd124e590c2432354b9fbe4d7c
Harry <hariharasethi89@gmail.com>
2025-06-29 20:20:07 +0530

    Array and Slice

--- booking-app/main.go
+++ booking-app/main.go
@@ -4,9 +4,9 @@ import "fmt" // fmt package Print() function belong here
 
 // main() is the entrypoint
 func main() {
-	const conferenceName = "Dev Conference"
+	conferenceName := "Dev Conference"
 	const conferenceTickets = 50
-	bookedTickets := 0 // go syntatical sugar (only for var)
+	var bookedTickets uint = 0 // go syntatical sugar (only for var)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Print("Get your ticket here to attend\n")
@@ -14,10 +14,22 @@ func main() {
 	fmt.Printf("Type of conferenceName: %T, Type of conferenceTickets: %T, Type of bookedTickets: %T\n",
 		conferenceName, conferenceTickets, bookedTickets)
 
+	// fmt.Println(&bookedTickets) // address of the variable (pointer)
+
+	// array for storing the booking details -> array declaration
+	// var bookingDetails = [50]string{"Aria", "Ben", "Catty"} // initilize with values
+	// var bookingDetails = [50]string{}
+	// var bookingDetails [50]string // declare a empty array
+
+	// slices is the abstraction of the array, but it is dynamic in size
+	// var bookingDetails = []string{}
+	// bookingDetails := []string{}
+	var bookingDetails []string
+
 	var firstName string // defining type of the variable
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	// taking user input
 	fmt.Println("Enter your first Name: ")
 	fmt.Scan(&firstName)
@@ -31,9 +43,18 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scanf("%d", &userTickets)
 
-	// fmt.Println(&bookedTickets) // address of the variable (pointer)
+	bookedTickets = bookedTickets + userTickets
+	// bookingDetails[0] = firstName + " " + lastName // string concatination
+	bookingDetails = append(bookingDetails, firstName+" "+lastName) // adding value to the slice
+
+	fmt.Println("Whole array ", bookingDetails)
+	fmt.Println("First element: ", bookingDetails[0])
+	fmt.Printf("Type of array: %T\n", bookingDetails)
+	fmt.Printf("Length of array: %v\n", len(bookingDetails))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will get confirmation email at %v.\n",
 		firstName, lastName, userTickets, email)
+
+	fmt.Printf("Total tickets: %d, Available tickets: %d\n", conferenceTickets, conferenceTickets-bookedTickets)
 
 }
