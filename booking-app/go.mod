ea400e1021527a8ed63a8c98902124c29894adb9
Harry <hariharasethi89@gmail.com>
2025-06-29 18:33:50 +0530

    First commit

---
+++ booking-app/go.mod
@@ -0,0 +1,6 @@
+module booking-app
+
+go 1.23.10
+
+
+// this is like a build file of go
---
+++ booking-app/main.go
@@ -0,0 +1,9 @@
+package main // everything in go is package
+
+import "fmt" // fmt package Print() function belong here
+
+// main() is the entrypoint
+func main() {
+	fmt.Println("Welcome to conference booking application")
+	fmt.Print("Get your ticket here to attend\n")
+}
