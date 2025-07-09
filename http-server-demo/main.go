f3acea8d082b6fd8a5a0b22d8a4a2b4cff2f6e33
Harry <hariharasethi89@gmail.com>
2025-07-09 20:03:19 +0530

    Make http-server handle json and post

--- http-server-demo/main.go
+++ http-server-demo/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -8,14 +9,17 @@ import (
 	"os"
 )
 
+type User struct {
+	Name string `json:"name"`
+	Age  uint   `json:"age"`
+}
+
+var users = make(map[int]User) // store the user in-memory
+
 func main() {
 	fmt.Println("HTTP SERVER")
-
-	// mux := http.NewServeMux();
-	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	io.WriteString(w, "Hello")
-	// })
-
+	const PORT string = ":8080"
+	mux := http.NewServeMux() // handle the routes
 	// read from file
 	bytedata, err := os.ReadFile("./index.html")
 
@@ -23,12 +27,49 @@ func main() {
 		panic(err)
 	}
 
-	handler := func(w http.ResponseWriter, req *http.Request) {
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
-		io.WriteString(w, string(bytedata))
+		w.Write(bytedata)
+	})
+
+	// post
+	mux.HandleFunc("POST /users", postUser)
+
+	mux.HandleFunc("GET /users", getUsers)
+
+	// any error this will handle
+	fmt.Println("Server is Listening to port ", PORT)
+	log.Fatal(http.ListenAndServe(PORT, mux))
+}
+
+func postUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
-	http.HandleFunc("/", handler)
-	// any error this will handle
-	log.Fatal(http.ListenAndServe(":4000", nil))
+	if user.Name == "" {
+		http.Error(w, "Name is required", http.StatusBadRequest)
+		return
+	}
+
+	// insert it to the user
+	users[len(users)+1] = user
+
+	w.WriteHeader(http.StatusNoContent)
+	io.WriteString(w, "User Accepted")
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	err := json.NewEncoder(w).Encode(users)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
